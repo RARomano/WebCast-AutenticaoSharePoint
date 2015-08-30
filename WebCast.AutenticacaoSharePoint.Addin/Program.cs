3d511a30bf1c69f2ecaa6cd12abb7137c37e1d79
RARomano <rodrigo@rodrigoromano.net>
2015-08-29 22:20:36 -0300

    added SharePoint Addin sample

---
+++ WebCast.AutenticacaoSharePoint.Addin/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCast.AutenticacaoSharePoint.Addin
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string siteUrl = "http://localhost";
+			Uri uri = new Uri(siteUrl);
+
+			string realm = TokenHelper.GetRealmFromTargetUrl(uri);
+
+			//Get the access token for the URL.  
+			//   Requires this app to be registered with the tenant
+			string accessToken = TokenHelper.GetAppOnlyAccessToken(
+				TokenHelper.SharePointPrincipal,
+				uri.Authority, realm).AccessToken;
+
+			using (var ctx = TokenHelper.GetClientContextWithAccessToken(uri.ToString(), accessToken))
+			{
+				/// codigo vai aqui
+				ctx.Load(ctx.Web);
+				var webTitle = ctx.Web.Title;
+
+				ctx.ExecuteQuery();
+
+				Console.Write(webTitle);
+
+			}
+		}
+	}
+}
